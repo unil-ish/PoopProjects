0e0e5cb4eae0127dc126f5c28f87422b50f2c6f1
Pedrooska <48513168+Pedrooska@users.noreply.github.com>
2022-05-10 14:50:13 +0200

    Quick Fix Scalar Indexes

--- 2021_22/theater_project/xml2df.py
+++ 2021_22/theater_project/xml2df.py
@@ -46,16 +46,17 @@ def xml2df(xml_path):
                 # Loops through "p" tags and adds to dataframe
                 for _, p in enumerate(tags_p):
                     p = p.text.strip()
-                    speaker_speech_df = pd.concat([speaker_speech_df,speaker_speech_df.from_dict({'speaker':speaker, 'speech':p})], ignore_index=True)
+                    speaker_speech_df = pd.concat([speaker_speech_df,speaker_speech_df.from_dict({'speaker':[speaker], 'speech':[p]})], ignore_index=True)
 
                 # Loops through "p" tags and adds to dataframe
                 for _, l in enumerate(tags_l):
                     l = l.text.strip()
-                    speaker_speech_df = pd.concat([speaker_speech_df,speaker_speech_df.from_dict({'speaker':speaker, 'speech':l})], ignore_index=True)
-
-        return speaker_speech_df
+                    speaker_speech_df = pd.concat([speaker_speech_df,speaker_speech_df.from_dict({'speaker':[speaker], 'speech':[l]})], ignore_index=True)
+        print(speaker_speech_df)
+        return speaker_speech_df       
 
     # Returns empty dataframe if the file could not be opened/found
     except IOError:
         print("Note : The supplied file was not found.")
         return pd.DataFrame(columns=['speaker', 'speech'])
+xml2df("./data/AbrahamLincolnbyJohnDrinkwater11172.xml")
