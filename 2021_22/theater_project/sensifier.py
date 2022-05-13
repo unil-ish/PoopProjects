cbcfa2a2616df176bda2b0cd03665816d9bcd929
Pedrooska <48513168+Pedrooska@users.noreply.github.com>
2022-05-13 11:03:19 +0200

    Importing 7AS senticnetStuff

--- 2021_22/theater_project/sensifier.py
+++ 2021_22/theater_project/sensifier.py
@@ -1,10 +1,13 @@
 """WORK IN PROGRESS"""
 
 from bs4 import BeautifulSoup
-
+from senticnetStuff import Senticnet_ops as sops
 
 
 file_path = '/Users/st-hilairecharlotte/Documents/GitHub/PoopProjects/2021_22/senticnet.py'
+
+print(sops.loadSenticnet(file_path))
+
 
 with open(file_path, 'r') as xml_file:
     xml_file = xml_file.read()
