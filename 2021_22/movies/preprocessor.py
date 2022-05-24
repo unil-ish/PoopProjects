2ff91c0f74cc8c36e61e6354f64bde5c33ca7b44
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2022-05-24 12:16:14 +0200

    preprocessor try

--- 2021_22/movies/preprocessor.py
+++ 2021_22/movies/preprocessor.py
@@ -7,8 +7,8 @@ class Preprocessor():
         with open(filepath, "r") as f :
             self.document = f.read()
 
-    def preprocess(self):
-        regex = re.compile("(?<=Synset\(')[^.]+")
+    def preprocess_speaker(self):
+        regex_speaker = re.compile("[A-Z]{2}.+")
         regex_dog = str(regex.findall(str(self.document)))
         print(regex_dog)
 
