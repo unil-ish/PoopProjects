ade6b29de1ae3980fb4caf82b03ce9872cd525cf
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2022-05-24 12:29:16 +0200

    preprocessor v2

--- 2021_22/movies/preprocessor.py
+++ 2021_22/movies/preprocessor.py
@@ -10,15 +10,14 @@ class Preprocessor():
     def preprocess_speaker(self):
         self.regex_speaker = re.compile("[A-Z]{2}.+")
         self.regex_speaker_in_doc = str(self.regex_speaker.findall(str(self.document)))
-
         return self.regex_speaker_in_doc
 
     def preprocess_speech(self):
         self.regex_speech = re.compile("^(?![A-Z]{2}).*$")
         self.regex_speech_in_doc = str(self.regex_speech.findall(str(self.document)))
-
         return self.regex_speech_in_doc
 
     def dataframe_speaker_speech(self):
         self.dataframe_speaker_speech = pd.DataFrame(self.regex_speaker_in_doc, self.regex_speech_in_doc,
                                                 columns=["Speaker", "Speech"])
+        return self.dataframe_speaker_speech
