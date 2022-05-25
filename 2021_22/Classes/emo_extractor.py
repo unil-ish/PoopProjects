c50ae2e434f0050d6305225915396ffdc73b5b8a
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2022-05-25 12:36:45 +0200

    add py files

---
+++ 2021_22/Classes/emo_extractor.py
@@ -0,0 +1,15 @@
+import senticnet
+
+class Emo_extractor():
+
+    def __init__(self, disambigauted_df):
+        self.emotions_df = disambigauted_df
+        self.list_1st_emotion = []
+        self.list_2nd_emotion = []
+
+    def extract_emotion(self):
+        self.emotions_df = self.emotions_df.astype('string')
+        for each in self.emotions_df:
+            for word, synset in each:
+                self.emotions_df['1st emotion'] = self.emotions_df.disambiguated[word].senticnet.senticnet[word][4]
+                self.emotions_df['2nd emotion'] = self.emotions_df.disambiguated[word].senticnet.senticnet[word][5]
