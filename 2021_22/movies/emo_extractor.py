ecd73f9dc29e163e83b835329b477a61ee653812
oliviav <olivia.verbrugge@unil.ch>
2022-05-26 17:33:27 +0200

    emo_extractor.py

--- 2021_22/movies/emo_extractor.py
+++ 2021_22/movies/emo_extractor.py
@@ -11,12 +11,12 @@ class Emo_Extractor():
         self.emotions_df = disambigauted_df
 
     def extract_emotion(self):
-        first_emotion=[]
-        second_emotion=[]
+        first_emotion = []
+        second_emotion = []
         self.emotions_df = self.emotions_df.astype('string')
         for elements in tqdm.tqdm(self.emotions_df['disambiguated']):
             for each in elements:
-                word=each[0].lower()
+                word = each[0].lower()
                 if word in senticnet.senticnet:
                     first_emotion.append(senticnet.senticnet[word[4]])
                     second_emotion.append(senticnet.senticnet[word[5]])
