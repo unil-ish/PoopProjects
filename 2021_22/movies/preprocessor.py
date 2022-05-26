4aa08f6dd9a7fd83a04108da5319b9def90d9016
semanticboy <davide.picca@unil.ch>
2022-05-26 12:27:34 +0200

    Fixed problems

--- 2021_22/movies/preprocessor.py
+++ 2021_22/movies/preprocessor.py
@@ -6,18 +6,14 @@ class Preprocessor():
     def __init__(self,filepath):
         with open(filepath, "r") as f :
             self.document = f.read()
-
-    def preprocess_speaker(self):
         self.regex_speaker = re.compile("[A-Z]{2}.+")
         self.regex_speaker_in_doc = str(self.regex_speaker.findall(str(self.document)))
-        return self.regex_speaker_in_doc
-
-    def preprocess_speech(self):
         self.regex_speech = re.compile("^(?![A-Z]{2}).*$")
         self.regex_speech_in_doc = str(self.regex_speech.findall(str(self.document)))
-        return self.regex_speech_in_doc
 
     def dataframe_speaker_speech(self):
+        self.df_regex_test = pd.DataFrame({"Speech":pd.Series(regex_speech_in_doc),"Speaker":pd.Series(regex_speaker_in_doc), 'Filename':pd.Series(filename),'Gender':pd.Series(gender)}
+                                     )
         self.dataframe_speaker_speech = pd.DataFrame(self.regex_speaker_in_doc, self.regex_speech_in_doc,
                                                 columns=["Speaker", "Speech"])
         return self.dataframe_speaker_speech
