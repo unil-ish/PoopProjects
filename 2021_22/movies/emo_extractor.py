eaab8c99595815914d33e3fde630c5ec3da09c6d
semanticboy <davide.picca@unil.ch>
2022-05-26 12:25:03 +0200

    Fixed problems

--- 2021_22/movies/emo_extractor.py
+++ 2021_22/movies/emo_extractor.py
@@ -1,13 +1,22 @@
-import senticnet
+import string
 
-class Emo_extractor():
+from . import senticnet
+
+class Emo_Extractor():
+
 
     def __init__(self, disambigauted_df):
         self.emotions_df = disambigauted_df
 
     def extract_emotion(self):
+        first_emotion=[]
+        second_emotion=[]
         self.emotions_df = self.emotions_df.astype('string')
-        for each in self.emotions_df:
+        for each in self.emotions_df['disambiguated']:
             for word, synset in each:
-                self.emotions_df['1st emotion'] = self.emotions_df.disambiguated[word].senticnet.senticnet[word][4]
-                self.emotions_df['2nd emotion'] = self.emotions_df.disambiguated[word].senticnet.senticnet[word][5]
+                first_emotion.append(senticnet.senticnet[word.lower()][4])
+                second_emotion.append(senticnet.senticnet[word.lower()][5])
+
+        self.emotions_df['2nd emotion'] = second_emotion
+        self.emotions_df['1st emotion'] = first_emotion
+        return self.emotions_df
