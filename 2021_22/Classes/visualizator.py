63763fafc6bd6537251f55a6bfe3d50e4a40eacc
Atnvgd <80701203+Atnvgd@users.noreply.github.com>
2022-05-29 12:02:39 +0200

    visualizator antoine

--- 2021_22/Classes/visualizator.py
+++ 2021_22/Classes/visualizator.py
@@ -1,8 +1,28 @@
-import numpy as np
+# Import the differnet packages that we need
 import seaborn as sns
-import matplotlib.pyplot as plt
 
+
+# Create the "Visualizator" class
 class Visualizator():
 
-    def __init__(self, emotions_df):
-        self.data = emotions_df
+    # This class needs an __init__ function
+    def __init__(self, df_speakers_speeches):
+        # With the dataframe we received after the preprocess operation
+        self.df_speeker_speech_vizualization = df_speakers_speeches
+
+    # Create a "gender_visualize" function
+    def gender_visualize(self):
+
+        # Create and return a visualization of the gender's occurrences in our datatable
+        sns.set_theme()
+        return sns.histplot(data=self.df_speeker_speech_vizualization, x="Gender")
+
+    # Create a "file_visualize" function
+    def file_vizualize(self):
+
+        # Create and return a visualization of the file's occurrences in our datatable
+        sns.set_theme()
+        return sns.histplot(data=self.df_speeker_speech_vizualization, x="Filename")
+
+
+
